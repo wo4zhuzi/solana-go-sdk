a8a098b823ced9df0e7dd09d043b494a9853cbf0
david <david@legenddigital.com.au>
2021-11-04 16:41:45 +0800

    health

--- client/rpc/get_health.go
+++ client/rpc/get_health.go
@@ -2,7 +2,6 @@ package rpc
 
 import (
 	"context"
-	"github.com/wo4zhuzi/solana-go-sdk/client"
 )
 
 func (s *RpcClient) GetHealth(ctx context.Context) (string, error) {
@@ -11,12 +10,6 @@ func (s *RpcClient) GetHealth(ctx context.Context) (string, error) {
 		Result string `json:"result"`
 	}{}
 	err := s.request(ctx, "getHealth", []interface{}{}, &res)
-	if err != nil {
-		return "", err
-	}
-
-	err = client.CheckRpcResult(res.GeneralResponse, err)
-
 	if err != nil {
 		return "", err
 	}

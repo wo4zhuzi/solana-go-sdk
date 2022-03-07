4bc8ba80057a6ef2b029681b297a3a2cc2fb5f32
david <david@legenddigital.com.au>
2022-03-07 14:18:42 +0800

    判断错误日志

--- client/rpc/get_block.go
+++ client/rpc/get_block.go
@@ -42,7 +42,7 @@ func (s *RpcClient) GetBlock(ctx context.Context, slot uint64, cfg GetBlockConfi
 		return GetBlockResponse{}, err
 	}
 
-	if res.Error != nil {
+	if res.Error != nil && res.Error.Code != 32007 {
 		return GetBlockResponse{}, fmt.Errorf("%v", res.Error)
 	}
 
